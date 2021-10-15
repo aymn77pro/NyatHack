3c8b17d2c2eae3637aa90297d6e92bc607ee87a3
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-15 19:03:33 +0300

    nyakHack Add code range

--- src/Game.kt
+++ src/Game.kt
@@ -3,14 +3,15 @@ fun main(){
  var healthpoint = 89
  val isBlessed = true
  val IsImmortal = false
- val aureIsVisbl = isBlessed && healthpoint >=50 || IsImmortal
+ val aureIsVisbl = isBlessed && healthpoint in 50..100 || IsImmortal
     //aure
-val aureColer  = if (aureIsVisbl){"green"}else{"none!"}
+val aureColer  = if (aureIsVisbl)"green" else "none!"
  println(aureColer)
- val heathstatus : String = if (healthpoint==100){ "$name is in excellent condition!"}
- else if (healthpoint>=90){"$name have few scratches!"}
- else if (healthpoint>=75){ if (isBlessed){"$name has some minor wounds but is healing quite quickly!"} else{ "$name has some minor wounds"}}
- else if (healthpoint>=15){"$name pretty hurt"}
- else{"$name is in awful condition!"}
+ val heathstatus : String = if (healthpoint==100) "$name is in excellent condition!"
+ else if (healthpoint in 90..99)"$name have few scratches!"
+ else if (healthpoint in 75..89)if (isBlessed)"$name has some minor wounds but is healing quite quickly!"
+ else    "$name has some minor wounds"
+ else if (healthpoint in 15..74)"$name pretty hurt"
+ else    "$name is in awful condition!"
 println(heathstatus)
 }
