1e91cfc84efe3e5fb4222cfdd590238ac04f9da2
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-15 18:41:26 +0300

    nyakHack Add new code(aure color)

--- src/Game.kt
+++ src/Game.kt
@@ -5,8 +5,8 @@ fun main(){
  val IsImmortal = false
  val aureIsVisbl = isBlessed && healthpoint >=50 || IsImmortal
     //aure
- if (aureIsVisbl){println("green")}
- else{println("none")}
+val aureColer  = if (aureIsVisbl){"green"}else{"none!"}
+ println(aureColer)
  val heathstatus : String = if (healthpoint==100){ "$name is in excellent condition!"}
  else if (healthpoint>=90){"$name have few scratches!"}
  else if (healthpoint>=75){ if (isBlessed){"$name has some minor wounds but is healing quite quickly!"} else{ "$name has some minor wounds"}}
